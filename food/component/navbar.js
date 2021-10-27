2b24996a8ba4cfe11b28ce08daf94a72b6e6a3cf
abhi103767 <avinash1234sharma1@gmail.com>
2021-10-27 17:20:13 +0530

    d

---
+++ food/component/navbar.js
@@ -0,0 +1,13 @@
+function navbar(){
+    return ` 
+    <h3><a href="home.html">Home</a></h3>
+
+    <div id="options">
+      <h3><a href="recipe_day.html">Recipe of the Day</a></h3>
+      <h3><a href="search.html">Search</a></h3>
+      <h3><a href="#">Sign In</a></h3>
+      <h3><a href="#">Sign Up</a></h3>
+    </div>`
+}
+
+export default navbar;
---
+++ pratice/components/show.js
@@ -0,0 +1,5 @@
+function showfn(){
+    return `<h1>Avinash</h1>`
+}
+
+export default showfn;
